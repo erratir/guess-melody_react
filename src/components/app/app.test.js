daf53b1ee2364dc4f86e880f7ed461a9c5d23ea8
erratir <mail4entry@gmail.com>
2019-11-09 02:50:52 +0300

    module3: Fix App snapshot test

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -2,12 +2,44 @@ import React from "react";
 import renderer from "react-test-renderer";
 import App from "./app.jsx";
 
+const mock = {
+  questions: [
+    {
+      type: `genre`,
+      genre: `rock`,
+      answers: [
+        {
+          src: ``,
+          genre: `jaz`,
+        },
+        {
+          src: ``,
+          genre: `pop`,
+        },
+        {
+          src: ``,
+          genre: `rock`,
+        },
+        {
+          src: ``,
+          genre: `jaz`,
+        },
+      ],
+    },
+  ],
+};
+
 it(`App correctly renders`, () => {
+
+  const {questions} = mock;
+
   const tree = renderer
     .create(
         <App
           maxTime={995}
-          maxMistakes={133}/>
+          maxMistakes={133}
+          questions ={questions}
+        />
     ).toJSON();
 
   expect(tree).toMatchSnapshot();
