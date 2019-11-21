24065298c678322bb998585f8f4b5857f4dac366
erratir <mail4entry@gmail.com>
2019-11-21 19:43:52 +0300

    module4: Add snapshot and e2e tests for AudioPlayer component

---
+++ src/components/audio-player/audio-player.e2e.test.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import Enzyme, {mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import AudioPlayer from './audio-player';
+
+Enzyme.configure({adapter: new Adapter()});
+
+const mock = {
+  src: `https://upload.wikimedia.org/wikipedia/commons/5/52/Anthem_of_Buryatia_%28Buryat_and_Russian_version%29.oga`,
+};
+
+describe(`AudioPlayer: Change state correctly when click on the button (play/pause):`, () => {
+  let player;
+  let button;
+  let buttonClickHandler;
+  let isPlaying = false;
+  const event = {preventDefault: () => {}};
+  global.window.HTMLMediaElement.prototype.pause = () => {};
+  global.window.HTMLMediaElement.prototype.play = () => {};
+
+  beforeEach(() => {
+
+    buttonClickHandler = jest.fn();
+
+    player = mount(
+        <AudioPlayer
+          isPlaying={isPlaying}
+          onPlayButtonClick={buttonClickHandler}
+          src={mock.src}
+        />
+    );
+
+    button = player.find(`.track__button`);
+
+    player.setState({isLoading: false});
+
+  });
+
+  describe(`before click on button`, () => {
+    it(`handler shouldn't be called`, () => {
+      expect(buttonClickHandler).toBeCalledTimes(0);
+    });
+  });
+
+  describe(`click on button`, () => {
+    beforeEach(() => {
+      button.simulate(`click`, event);
+    });
+
+    it(`handler should be called once`, () => {
+      expect(buttonClickHandler).toBeCalledTimes(1);
+    });
+    it(`state "isPlaying" should true`, () => {
+      expect(player.state().isPlaying).toBe(true);
+    });
+
+    describe(`click on button again`, () => {
+      beforeEach(() => {
+        button.simulate(`click`, event);
+      });
+      it(`handler should be called twice`, () => {
+        expect(buttonClickHandler).toBeCalledTimes(2);
+      });
+      it(`state "isPlaying" should be false`, () => {
+        expect(player.state().isPlaying).toBe(false);
+      });
+    });
+  });
+
+});
---
+++ src/components/audio-player/audio-player.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import AudioPlayer from './audio-player';
+
+
+const mock = {
+  isPlaying: false,
+  onPlayButtonClick: jest.fn(),
+  src: ``,
+};
+
+it(`AudioPlayer correctly renders`, () => {
+  const createNodeMock = (element) => {
+    if (element.type === `audio`) {
+      return {
+        src: ``
+      };
+    }
+    return null;
+  };
+
+  const player = renderer
+    .create(<AudioPlayer
+      isPlaying={mock.isPlaying}
+      onPlayButtonClick={mock.onPlayButtonClick}
+      src={mock.src}
+    />, {createNodeMock})
+    .toJSON();
+
+  expect(player).toMatchSnapshot();
+});
