6ed9c79bf1ed29ab1480388c9cc8e7526d685dba
erratir <mail4entry@gmail.com>
2020-01-09 23:31:25 +0300

    module6: connect redux-dev-tool

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,9 @@ import {reducer} from "./reducer.js";
 import App from "./components/app/app.jsx";
 import {maxTime, maxMistakes, questions} from "./mocks/game.js";
 
-const store = createStore(reducer);
+const store = createStore(reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const init = (gameQuestions) => {
   ReactDOM.render(<Provider store={store}><App
