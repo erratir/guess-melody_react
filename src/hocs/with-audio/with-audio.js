5ece36447b5cc42946f2022ee286298fe027fe64
erratir <mail4entry@gmail.com>
2020-01-13 12:42:35 +0300

    module6: perf: reduce frequency of re-rendering audio-player
    optimize performance: reduce frequency of re-rendering audio-player
    in HOC `with-audio`
    update progress once per second

--- src/hocs/with-audio/with-audio.js
+++ src/hocs/with-audio/with-audio.js
@@ -60,7 +60,7 @@ const withAudio = (Component) => {
       });
 
       audio.ontimeupdate = () => this.setState({
-        progress: audio.currentTime
+        progress: Math.floor(audio.currentTime)
       });
     }
 
